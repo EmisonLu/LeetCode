359822d3eb3a93e3f01542fb6049e43822e272aa
EmisonLu <54395432+EmisonLu@users.noreply.github.com>
2020-03-09 23:28:09 +0800

    2 solutions ，map

--- C++/1.两数之和.cpp
+++ C++/1.两数之和.cpp
@@ -5,17 +5,21 @@ using namespace std;
 
 class Solution {
 public:
-//    vector<int> twoSum(vector<int> &nums, int target) {
-//        int size = nums.size();
-//        for (int i = 0; i < size - 1; ++i) {
-//            for (int j = i + 1; j < size; ++j) {
-//                if (nums[i] + nums[j] == target) {
-//                    return {i, j};
-//                }
-//            }
-//        }
-//        return {};
-//    }
+
+//解法1
+   vector<int> twoSum(vector<int> &nums, int target) {
+       int size = nums.size();
+       for (int i = 0; i < size - 1; ++i) {
+           for (int j = i + 1; j < size; ++j) {
+               if (nums[i] + nums[j] == target) {
+                   return {i, j};
+               }
+           }
+       }
+       return {};
+   }
+    
+//解法2
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> a;//建立hash表存放数组元素
         vector<int> b(2, -1);//存放结果
